772210fb21dbaa6a282918aeac9825f7cec3fe21
Md Raise Azam <raise.azam@gmail.com>
2019-01-31 11:41:43 +0530

    powershell assignment on 31-01-2019

---
+++ ExceptionHandling.ps1
@@ -0,0 +1,20 @@
+﻿
+function Test1
+{
+param([int]$x,[int]$y)
+try
+{
+$z = $x / $y
+[Math]::Floor($z)
+# Write-Host $z
+}
+catch
+{
+ $e=$_.Exception.Message
+ Write-Host $e
+# Write-Error $_
+}
+}
+[int]$x =  Read-Host "Enter Numerator "
+[int]$y = Read-Host  "Enter Denominator "
+Test1 $x $y
